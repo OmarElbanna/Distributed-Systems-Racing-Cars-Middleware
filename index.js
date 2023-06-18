4fb34fe27ec8aa7bc2ab2fa939566d7659670963
OmarElbanna <elbanna16@gmail.com>
2023-06-18 13:50:02 +0300

    Getting players and rooms number from MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://omarmayousef:G7IQyLiT1OKcn0Lj@cluster0.shg8nan.mongodb.net/?retryWrites=true&w=majority";
 
-app.get("/", (req, res) => {
-    res.send("I will be shown on the Browser");
-    console.log("I will be shown on the Terminal");
+
+
+app.get("/rooms", async (req, res) => {
+
+    const client = await MongoClient.connect(uri);
+    console.log("connected");
+    const db = client.db('analysis_db');
+    const collection = db.collection('analysis_coll');
+    const rooms= await collection.findOne({item:'rooms'});
+    console.log(rooms.number);
+    client.close();
+    res.send({'rooms':rooms.number});
+    
 });
+
+app.get("/players", async (req, res) => {
+
+    const client = await MongoClient.connect(uri);
+    console.log("connected");
+    const db = client.db('analysis_db');
+    const collection = db.collection('analysis_coll');
+    const players= await collection.findOne({item:'players'});
+    console.log(players.number);
+    client.close();
+    res.send({'players':players.number});
+
+
+});
+
+
 app.listen(3000);
