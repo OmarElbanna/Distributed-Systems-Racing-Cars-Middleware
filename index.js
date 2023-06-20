3ff56c4617b63e428f168dde0185a68e85c9697c
OmarElbanna <elbanna16@gmail.com>
2023-06-20 12:55:29 +0300

    Added new route to increment website views

--- index.js
+++ index.js
@@ -51,4 +51,15 @@ app.get("/update", async (req, res) => {
 });
 
 
+app.get("/views", async (req, res) => {
+
+    const client = await MongoClient.connect(uri);
+    console.log("connected");
+    const db = client.db('analysis_db');
+    const collection = db.collection('analysis_coll');
+    const result= await collection.findOneAndUpdate({item:'views'},{$inc:{number:1}});
+    client.close();
+    res.send({'status':'True'});
+});
+
 app.listen(3000);
